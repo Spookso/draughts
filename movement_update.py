246f004a9e6a8acca954939164108d648c7cb274
Spookso <52243767+Spookso@users.noreply.github.com>
2020-10-19 16:00:53 +0800

    Update movement_update.py

--- movement_update.py
+++ movement_update.py
@@ -1,14 +1,15 @@
+import time
 # Checks that the piece is moving the right way for its type
 def direction_check(board, start_row, start_col, end_row):
 
     # if it is a normal piece, checks it is moving forward only
     if board[start_row][start_col] == 1:
         if end_row > start_row:
-            print("Wrong Direction for Piece Type")
+            # print("Wrong Direction for Piece Type")
             return False
     if board[start_row][start_col] == 3:
         if end_row < start_row:
-            print("Wrong Direction for Piece Type")
+            # print("Wrong Direction for Piece Type")
             return False
 
     return True
@@ -40,7 +41,7 @@ def move_check(board, start_row, start_col, end_row, end_col):
         else:
             direction = -1
 
-        print("start row:", start_row, "end row:", end_row, "start col:", start_col, "end col:", end_col, "direction:", direction, "side:", side)
+        # print("start row:", start_row, "end row:", end_row, "start col:", start_col, "end col:", end_col, "direction:", direction, "side:", side)
 
         # if the piece is trying to move to a square on the board
         if -1 < end_row < 8 and -1 < end_col < 8:
@@ -72,7 +73,7 @@ def move(board, start_row, start_col, end_row, end_col, direction, side):
     board[start_row + direction][start_col + side] = 0
     board[end_row][end_col] = board[start_row][start_col]
     board[start_row][start_col] = 0
-    print("Moved.")
+    # print("Moved.")
 
     return board
 
@@ -107,60 +108,122 @@ def double_move_check(board, row, col, direction, side):
 
     return repeat, row, col, work_row, work_col
 
+
+
 class leaf():
-    def __init__(self, own_board, own_move, own_score, parent, team, forced):
-        self.own_board = own_board
-        self.own_move = own_move
-        self.own_score = own_score
-        self.parent = parent
-        self.team = team
+    def __init__(self, board, move_list, forced, depth, team):
+        self.board = board
+        self.move_list = move_list
         self.forced = forced
         self.child_list = []
-        self.best_child = -10000
+        self.depth = depth + 1
+        self.best_child = 0
+        self.team = team
 
-    def team_swap(self):
-        if self.team == 1:
-            return 3
-        return 1
+    def team_change(self):
+        if self.team == [3, 4]:
+            return [1, 2]
+        return [3, 4]
+
+    def find_board(self):
+        self.board = [
+            [0, 3, 0, 3, 0, 3, 0, 3],
+            [3, 0, 3, 0, 3, 0, 3, 0],
+            [0, 3, 0, 3, 0, 3, 0, 3],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 1, 0, 1, 0, 1, 0],
+            [0, 1, 0, 1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 1, 0, 1, 0]
+        ]
+        for i in range(0, len(self.move_list)):
+            self.board = move(self.board, self.move_list[i][0], self.move_list[i][1], self.move_list[i][2], self.move_list[i][3], self.move_list[i][4], self.move_list[i][5])
+            self.king_check()
+
+        return self.board
+
+    def king_check(self):
+        num = 0
+        for piece in self.board[0]:
+            if piece == 1:
+                self.board[0][num] = 2
+            num += 1
+        num = 0
+        for piece in self.board[7]:
+            if piece == 3:
+                self.board[7][num] = 4
+            num += 1
 
     def calculate_moves(self):
+        self.board = self.find_board()
         if self.forced == '':
             start_row = 0
-            for row in self.own_board:
+            for row in self.board:
                 start_col = 0
                 for piece in row:
-                    if piece == self.team or piece == self.team + 1:
+                    if piece in self.team:
                         for end_row in range(0, 8):
                             for end_col in range(0, 8):
-                                self.clone(start_row, start_col, end_row, end_col)
+                                if move_check(self.board, start_row, start_col, end_row, end_col)[0]:
+                                    self.clone(start_row, start_col, end_row, end_col)
                     start_col += 1
                 start_row += 1
         else:
             start_row, start_col = self.forced
-            if double_move_check(self.own_board, start_row, start_col, 1, 1)[0]:
-                end_row = double_move_check(self.own_board, start_row, start_col, 1, 1)[3]
-                end_col = double_move_check(self.own_board, start_row, start_col, 1, 1)[4]
-                self.clone(start_row, start_col, end_row, end_col)
+            if double_move_check(self.board, start_row, start_col, 1, 1)[0]:
+                time.sleep(2)
+                print("DOUBLE FOUND")
+                end_row = double_move_check(self.board, start_row, start_col, 1, 1)[3]
+                end_col = double_move_check(self.board, start_row, start_col, 1, 1)[4]
+                if move_check(self.board, start_row, start_col, end_row, end_col)[0]:
+                    self.clone(start_row, start_col, end_row, end_col)
 
     def clone(self, start_row, start_col, end_row, end_col):
-        moves = [start_row, start_col, end_row, end_col, move_check(self.own_board, start_row, start_col, end_row, end_col)[2], move_check(self.own_board, start_row, start_col, end_row, end_col)[3]]
-        new_board = move(self.own_board, self.own_move[0], self.own_move[1], self.own_move[2], self.own_move[3], self.own_move[4], self.own_move[5])
-        score = 0
-        if move_check(self.own_board, start_row, start_col, end_row, end_col)[4]:
+        moves = self.move_list
+        moves.append([start_row, start_col, end_row, end_col, move_check(self.board, start_row, start_col, end_row, end_col)[2], move_check(self.board, start_row, start_col, end_row, end_col)[3]])
+        # bit confused about here; shouldn't it be moves[0], moves[1], etc instead of own_move
+        # as you are performing the new moves
+        #---------------------------------------------------
+        # something is fundamentally broken here
+        # board is not being updated correctly; the boards that the twigs have are not 4 moves ahead,
+        # they are the current board but with each twig losing a piece
+        # THIS WHOLE THING needs to be like completely re-wrtten
+        # If no solution to the board loss is found ai might all need to be scrapped
+        # good luck
+        self.board = self.find_board()
+        if move_check(self.board, start_row, start_col, end_row, end_col)[4]:
             force = [start_row, start_col]
         else:
             force = ''
-        for row in new_board:
+        new_board = move(self.board, self.move_list[-1][0], self.move_list[-1][1], self.move_list[-1][2], self.move_list[-1][3], self.move_list[-1][4], self.move_list[-1][5])
+        if self.depth != 4:
+            self.child_list.append(leaf(new_board, moves, force, self.depth, self.team_change()))
+        else:
+            self.child_list.append(twig(new_board, moves, force, self.depth, self.team_change()))
+
+class twig(leaf):
+    # issue here is that the board is deleted when things happen
+    # so each twig has less board
+    # and can't just use the update function because this is the board three moves in advance
+    # don't know how to fix this without a major rework, or firguring out why the board keeps changing itself
+    # when it shouldn't
+    def score_self(self):
+        self.board = self.find_board()
+        print(self.board)
+        score = 0
+        empty = True
+        for row in self.board:
             for piece in row:
-                if piece != self.team and piece != self.team + 1:
-                    score -= 3
-                elif piece == self.team + 1:
-                    score += 2
-                elif piece == self.team:
+                if piece == 3:
                     score += 1
-        self.child_list.append(leaf(new_board, moves, score, self, self.team_swap(), force))
-
-
-class petal(leaf):
-    def score(self):
-        pass
+                    empty = False
+                elif piece == 4:
+                    score += 2
+                    empty = False
+                elif piece == 1:
+                    score -= 3
+                elif piece == 2:
+                    score -= 4
+        if empty:
+            score = -10000000000
+        self.score = score
